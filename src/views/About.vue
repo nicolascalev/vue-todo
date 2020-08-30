90f78ef1a8d65d48aa7bc9a2e18332e897208a27
Nicolas <guillen_nicolas@live.com>
2020-08-30 17:26:58 -0600

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,7 @@
       <hr>
       <h3 class="mt-5">Check my GitHub profile</h3>
       <p>I have some other projects and content, it's worth checking out</p>
-      <p class="text-muted">@nicolascalev</p>
+      <a href="https://github.com/nicolascalev" class="text-muted">@nicolascalev</a>
       <hr>
       <h3 class="mt-5">HTTP Requests</h3>
       <p>You guessed it! I'm using Axios to perform http requests to a fake api called JSON Placeholder</p>
